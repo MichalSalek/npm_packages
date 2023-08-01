0b66c52d0a7f7342423d9d17c6985b7efeea6d8b
Michał Sałek <contact@michalsalek.pl>
2023-08-02 00:56:29 +0200

    verified sender handling

--- email/src/data/email.ts
+++ email/src/data/email.ts
@@ -4,9 +4,9 @@ import sendgrind from '@sendgrid/mail'
 export const setApiKey = (API_KEY: string) => sendgrind.setApiKey(API_KEY)
 
 
-export const sendEmail = (msg: any) => {
+export const sendEmail = (msg: any, VERIFIED_SENDER: string) => {
     sendgrind
-        .send(msg)
+        .send({...msg, from: VERIFIED_SENDER})
         .then(() => {
             console.log('Email sent')
         })
