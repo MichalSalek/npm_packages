3a3864ba6cd4e0dc1f2e1b62c81803e3eec0a5b0
Michał Sałek <contact@michalsalek.pl>
2023-08-02 14:24:11 +0200

    stable version of mailing layer

--- email/src/data/email.ts
+++ email/src/data/email.ts
@@ -1,17 +1,21 @@
-import sendgrind from '@sendgrid/mail'
+import sendgrid from '@sendgrid/mail'
 
 
-export const setApiKey = (API_KEY: string) => sendgrind.setApiKey(API_KEY)
 
+
+export const setApiKey = (API_KEY: string) => sendgrid.setApiKey(API_KEY)
+
+
+// 'from' key is mandatory in sendgrid and must be verified at sendgrid website.
 
 export const sendEmail = (msg: any, VERIFIED_SENDER: string) => {
-    sendgrind
+    sendgrid
         .send({...msg, from: VERIFIED_SENDER})
         .then(() => {
-            console.log('Email sent')
+            console.log('@msalek/emails: Email sent.')
         })
         .catch((error: any) => {
+            console.error('@msalek/emails: Error:')
             console.error(error)
         })
 }
-
