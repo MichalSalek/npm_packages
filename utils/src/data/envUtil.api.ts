dec54f036605405485a20586f7103ee0655a4ac7
Michał Sałek <contact@michalsalek.pl>
2023-08-03 21:17:25 +0200

    utils upgrade

--- utils/src/data/envUtil.api.ts
+++ utils/src/data/envUtil.api.ts
@@ -1,2 +1,8 @@
 
-export const isProductionEnv = (): boolean => process.env.NODE_ENV === 'production'
+export const isProductionEnv = (byUrl: boolean): boolean => {
+    if (byUrl) {
+        return !(window.location.host.includes('localhost') || window.location.host.includes('127.0.0'))
+    }
+
+    return process.env.NODE_ENV === 'production'
+}
