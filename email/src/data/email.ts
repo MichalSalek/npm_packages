24bcb799f979df3aa085110aa50cb9ecf1218e6c
Michał Sałek <contact@michalsalek.pl>
2023-08-09 16:26:37 +0200

    more fit payload sendgrid integration

--- email/src/data/email.ts
+++ email/src/data/email.ts
@@ -15,9 +15,16 @@ export type SendEmail = {
 export const sendEmail = async (payload: SendEmail, VERIFIED_SENDER: string): Promise<void> => {
 
     try {
-        await sendEmailSendGrid({...payload, from: VERIFIED_SENDER})
+        await sendEmailSendGrid({
+            from: VERIFIED_SENDER,
+            html: payload.html,
+            to: payload.to,
+            text: payload.text,
+            subject: payload.subject
+        })
 
-        console.log('@msalek/emails: Email sent.')
+        console.log('@msalek/emails: Email sent.',
+            `from: ${VERIFIED_SENDER}, to: ${payload.to}, subject: ${payload.subject}`)
 
         return Promise.resolve()
     } catch (error: any) {
