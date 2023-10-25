a21d7eb2cca983a925519ea88aca91d871deaeef
Michał Sałek <contact@michalsalek.pl>
2023-10-25 10:31:49 +0200

    only2DigitsFloat new number util

--- utils/src/data/numberUtil.api.ts
+++ utils/src/data/numberUtil.api.ts
@@ -1,2 +1,16 @@
-export const randomIntFromNumbersRange = (minRange: number, maxRange: number) =>
-    Math.floor(Math.random() * (maxRange - minRange + 1) + minRange)
+export const randomIntFromNumbersRange = (minRange: number, maxRange: number): number => {
+    if (Number.isNaN(Number(minRange))) {
+        throw Error('minRange is not a number.')
+    }
+    if (Number.isNaN(Number(maxRange))) {
+        throw Error('maxRange is not a number.')
+    }
+    return Math.floor(Math.random() * (maxRange - minRange + 1) + minRange)
+}
+
+export const only2DigitsFloat = (data: number): number => {
+    if (Number.isNaN(Number(data))) {
+        throw Error('Input data is not a number.')
+    }
+    return parseFloat(data.toFixed(2))
+}
--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,9 +1,9 @@
 import { freezeThreadAndWait }                                                                             from './data/asyncUtil.api'
 import { getDateNowInString }                                                                              from './data/dateAndTimeUtil.api'
 import { isProductionEnv }                                                                                 from './data/envUtil.api'
-import { randomIntFromNumbersRange }                                                                       from './data/numberUtil.api'
+import { only2DigitsFloat, randomIntFromNumbersRange }                                                     from './data/numberUtil.api'
 import { getCapitalizedString }                                                                            from './data/stringUtil.api'
-import { ValueOf, isUndefinedType }                                                                                 from './data/typesUtil.api'
+import { isUndefinedType, ValueOf }                                                                        from './data/typesUtil.api'
 import { getQueryStringWithoutLastMergeChar, getURLParameters, getURLWithoutSearch, setNewURLImmediately } from './data/urlUtil.api'
 import { asyncScrollToTop }                                                                                from './data/windowUtil.api'
 
@@ -15,6 +15,7 @@ export {
     getDateNowInString,
     isProductionEnv,
     randomIntFromNumbersRange,
+    only2DigitsFloat,
     isUndefinedType,
     setNewURLImmediately,
     getURLParameters,
