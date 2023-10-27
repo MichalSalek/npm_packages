ddc9d145196889f202bb7a8f184bc3ab392b4bf2
Michał Sałek <contact@michalsalek.pl>
2023-10-27 19:53:48 +0200

    revert - get number, give number without magic

--- utils/src/data/numberUtil.api.ts
+++ utils/src/data/numberUtil.api.ts
@@ -8,10 +8,9 @@ export const randomIntFromNumbersRange = (minRange: number, maxRange: number): n
     return Math.floor(Math.random() * (maxRange - minRange + 1) + minRange)
 }
 
-export const only2DigitsFloat = (data: number | string): number => {
+export const only2DigitsFloat = (data: number): number => {
     if (Number.isNaN(Number(data))) {
-        console.error('only2DigitsFloat', 'Input data is not a number.', typeof data, data)
-        return Number(data)
+        throw Error(`'only2DigitsFloat', 'Input data is not a number.', ${typeof data}, ${data}`)
     }
     return parseFloat(Number(data).toFixed(2))
 }
