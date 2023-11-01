8fe6933a44a6a8b782ba7a1b810cf699fb179123
Michał Sałek <contact@michalsalek.pl>
2023-11-01 21:01:09 +0100

    debounce update

--- utils/src/data/asyncUtil.api.ts
+++ utils/src/data/asyncUtil.api.ts
@@ -1,14 +1,12 @@
 export const freezeThreadAndWait = async (delay: number): Promise<void> =>
-  new Promise(function(resolve) {
-    setTimeout(resolve, delay)
-  })
+    new Promise(function (resolve) {
+        setTimeout(resolve, delay)
+    })
 
-export const debounce = {
-    ...{
-        timeoutId: 0,
-        fn:        (mainFunction: () => void, delay: number = 500) => {
-            window.clearTimeout(debounce.timeoutId)
-            debounce.timeoutId = window.setTimeout(() => mainFunction(), delay)
-        }
+export const debounce = function (mainFunction: () => void, delay: number = 500) {
+    let timeoutId = 0
+    return () => {
+        window.clearTimeout(timeoutId)
+        timeoutId = window.setTimeout(() => mainFunction(), delay)
     }
 }
