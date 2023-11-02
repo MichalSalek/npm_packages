3978e4ce5f4bf7e9b0e0ce1b4cc6de9e1ec29fbe
Michał Sałek <contact@michalsalek.pl>
2023-11-02 16:57:28 +0100

    much simply only2digits float

--- utils/src/data/numberUtil.api.ts
+++ utils/src/data/numberUtil.api.ts
@@ -8,19 +8,9 @@ export const randomIntFromNumbersRange = (minRange: number, maxRange: number): n
     return Math.floor(Math.random() * (maxRange - minRange + 1) + minRange)
 }
 
-export const only2DigitsFloat = (data: number, round?: 'up' | 'down' | 'mathematically'): number | null => {
+export const only2DigitsFloat = (data: number): number | null => {
     if (Number.isNaN(Number(data))) {
         return null
     }
-    const sureNumber = Number(data)
-
-    switch (round) {
-        case 'mathematically':
-            return Number(Math.round(sureNumber).toFixed(2))
-        case 'up':
-            return Number(Math.ceil(sureNumber).toFixed(2))
-        case 'down':
-        default:
-            return Number(Math.floor(sureNumber).toFixed(2))
-    }
+    return Number(data.toFixed(2))
 }
