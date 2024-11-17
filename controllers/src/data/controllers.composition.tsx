139d5bb7d891afe7468a3d97f5adf05cc8ef6076
Michał Sałek <contact@michalsalek.pl>
2024-11-17 02:36:50 +0100

    cleanup

--- controllers/src/data/controllers.composition.tsx
+++ controllers/src/data/controllers.composition.tsx
@@ -12,74 +12,89 @@ export type NoReturnValueFunctions = NoReturnValueFunction[]
 
 
 type Props = {
-    children?: ReactNode
-    autostartFunctions?: NoReturnValueFunctions
-    userInteractionFunctions?: NoReturnValueFunctions
-    hookControllers?: NoReturnValueFunctions
-    JSXControllers?: ControllersWithJSX
+  children?: ReactNode
+  autostartFunctions?: NoReturnValueFunctions
+  userInteractionFunctions?: NoReturnValueFunctions
+  hookControllers?: NoReturnValueFunctions
+  JSXControllers?: ControllersWithJSX
 }
 
-export const ControllersComposition = (
-    {
-        children = undefined,
-        autostartFunctions = [],
-        userInteractionFunctions = [],
-        JSXControllers = [],
-        hookControllers = []
-    }: Props): ReactElement => {
+export const ControllersComposition = ({
+  children = undefined,
+  autostartFunctions = [],
+  userInteractionFunctions = [],
+  JSXControllers = [],
+  hookControllers = []
+}: Props): ReactElement => {
 
-    // Hooks
-    //
-    hookControllers?.map((controller: NoReturnValueFunction) => controller())
-
-
-    const runOnce = useRef(false)
-    useEffect(() => {
-        if (runOnce.current) return () => void undefined
-        runOnce.current = true
-        freezeThreadAndWait(1).then(async () => {
-
-            // Autostart
-            //
-          await Promise.all(autostartFunctions)
-            // autostartFunctions?.forEach((func: NoReturnValueFunction) => {
-            //     func()
-            // })
+  // Hooks
+  //
+  hookControllers?.map((controller: NoReturnValueFunction) => controller())
 
 
-            // User Interaction start
-            //
-            const userEvents = ['scroll', 'keydown', 'pointerdown', 'pointermove', 'touchstart']
-            let innerRunOnce = false
-            const callbackClosure = (): void => {
-                if (innerRunOnce) return void undefined
-                innerRunOnce = true
-                setTimeout(() => {
-                    userInteractionFunctions?.forEach((func: NoReturnValueFunction) => {
-                        func()
-                    })
-                }, 1000)
-                userEvents.forEach((eventName) => document?.removeEventListener(eventName, callbackClosure))
+  const runOnce = useRef(false)
+  useEffect(
+    () => {
+      if (runOnce.current) {
+        return () => void undefined
+      }
+      runOnce.current = true
+      freezeThreadAndWait(1)
+        .then(async () => {
+
+          // Autostart
+          //
+          autostartFunctions?.forEach((func: NoReturnValueFunction) => {
+            func()
+          })
+
+
+          // User Interaction start
+          //
+          const userEvents = [ 'scroll',
+                               'keydown',
+                               'pointerdown',
+                               'pointermove',
+                               'touchstart' ]
+          let innerRunOnce = false
+          const callbackClosure = (): void => {
+            if (innerRunOnce) {
+              return void undefined
             }
-            userEvents.forEach((eventName) => document?.addEventListener(eventName, callbackClosure))
+            innerRunOnce = true
+            setTimeout(
+              () => {
+                userInteractionFunctions?.forEach((func: NoReturnValueFunction) => {
+                  func()
+                })
+              },
+              1000)
+            userEvents.forEach((eventName) => document?.removeEventListener(
+              eventName,
+              callbackClosure))
+          }
+          userEvents.forEach((eventName) => document?.addEventListener(
+            eventName,
+            callbackClosure))
 
         })
-        return () => void undefined
-    }, [])
+      return () => void undefined
+    },
+    [])
 
 
-    // JSX Controllers
-    //
-    return <>
-        {JSXControllers?.map((controller: ControllerWithJSX, index: number) => {
-            const calledController = controller()
-            if (!calledController) {
-                console.error('@msalek/controllers:' + ' Empty controller tried be called as JSX.')
-                return null
-            }
-            return <aside key={index}>{calledController}</aside>
-        })}
+  // JSX Controllers
+  //
+  return <>
+    {JSXControllers?.map((controller: ControllerWithJSX, index: number) => {
+      const calledController = controller()
+      if (!calledController) {
+        console.error('@msalek/controllers:' + ' Empty controller tried be called as JSX.')
+        return null
+      }
+      return <aside key={index}>{calledController}</aside>
+    })}
 
-        {children}
-    </>
+    {children}
+  </>
 }
